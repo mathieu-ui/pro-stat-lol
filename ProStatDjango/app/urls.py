18ed7dfa30071390fce09d52944684b4f956f86b
Mathieu Puig <97044385+mathieu-ui@users.noreply.github.com>
2025-01-21 15:29:50 +0100

    Modif feature Matchs (#10)
    Co-authored-by: Thomas EDEL <72469757+thomasedel@users.noreply.github.com>

--- ProStatDjango/app/urls.py
+++ ProStatDjango/app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('masteries', masteries),
     path('match', match_view, name='match'),
+    path('index', index),
+    path('game/<str:game_code>', match_overview),
+    path('action', redirector),
 ]
