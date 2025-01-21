d32dba58d92567eadf52bbec54ddc77a6ec86d07
Thomas EDEL <72469757+thomasedel@users.noreply.github.com>
2025-01-21 20:57:09 +0100

    Modif CSS

--- ProStatDjango/app/urls.py
+++ ProStatDjango/app/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('masteries', masteries),
-    path('match', match_view, name='match'),
+    #path('masteries', masteries),
+    #path('match', match_view, name='match'),
     path('index', index),
     path('game/<str:game_code>', match_overview),
     path('action', redirector),
